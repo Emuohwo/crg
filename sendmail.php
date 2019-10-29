e2c250e1dc2507d2ff9bddd413ac97b7ab41ecce
Warri <warriisaac@outlook.com>
2019-10-29 10:44:06 +0100

    This is the first push for the project

---
+++ sendmail.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$phone = $_POST['Phone'];
+//$call = $_POST['call'];
+//$type = $_POST['type'];
+$subj = $_POST['Subject'];
+$message = $_POST['Message'];
+$formcontent=" From: $name \n Phone: $phone \n Message: $message";
+$recipient = "YOUREMAIL@HERE.COM";
+//$recipient = "warriemuohwo@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Message sent Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
